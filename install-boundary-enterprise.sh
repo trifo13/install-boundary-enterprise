aa81e135b0ac8aeb42e4f5181bf2ddcc1ab6637b
trifo13 <trifo13@gmail.com>
2024-03-27 10:30:50 +0200

    First commit

---
+++ install-boundary-enterprise.sh
@@ -0,0 +1,7 @@
+# Detect OS:
+
+# Ubuntu/Debian install:
+curl -fsSL https://apt.releases.hashicorp.com/gpg | sudo apt-key add -
+sudo apt-add-repository "deb [arch=amd64] https://apt.releases.hashicorp.com $(lsb_release -cs) main"
+sudo apt update
+sudo apt install boundary-enterprise
